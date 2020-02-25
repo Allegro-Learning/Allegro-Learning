86db72494f63a0fcfc2b56af2a6583174bece2a1
Lucas <lp1935@nyu.edu>
2020-02-25 16:25:01 -0500

    new headers + midifile dependency

---
+++ Classes/Song.h
@@ -0,0 +1,15 @@
+
+#ifndef __SONG_H__
+#define __SONG_H__
+
+#include "cocos2d.h"
+
+class Song {
+private:
+    
+public:
+    virtual bool init();
+
+}
+
+#endif
---
+++ Classes/SongLibrary.h
@@ -0,0 +1,16 @@
+
+#ifndef __SONG_LIBRARY_H__
+#define __SONG_LIBRARY_H__
+
+#include "cocos2d.h"
+
+class SongLibrary {
+public:
+    virtual bool init();
+
+    Vector getSongNames();
+
+    
+}
+
+#endif
